cf96add9997b83b12e8d9be4bcce43670672bf59
pielube <pietrolubello@gmail.com>
2022-03-03 14:54:05 +0100

    small changes to run

--- RUN.py
+++ RUN.py
@@ -1,43 +1,48 @@
 from REC import REC
 from Economy import NPV
 import Post_processing as pp
-from Input import*
+from Input import *
 import time
 
-" RECS Renewable Energy Communities Simulator "
+"""
+MESSpy - Run
+"""
 
 study_case = 'study case' # str name for results file.pkl
 reference_case = 'reference case' # str name for results file.pkl
 
-#%% study case simulation 
-time1 = time.time()  
-
+time1 = time.time()
+ 
+print('Creating structure')
+# Creating initial structure
 rec = REC(structure,simulation_years) # create REC structure
 #rec.reset() # reset REC energy balances
 
-time2 = time.time() 
- 
+time2 = time.time()
+
+print('Running the model')
+# Running the model
 rec.REC_energy_simulation() # simulate REC structure
 rec.save(study_case) # save results in 'study_case.pkl'
 pp.total_balances(study_case)
 # pp.SOC_plot(study_case)
 
-time3 = time.time()  
-
-#%% reference case simulation (run only if changed)
+time3 = time.time()
+  
+print('Economic analysis')
+# Reference case simulation (run only if changed)
 rec0 = REC(structure0,simulation_years) # create REC
 rec0.REC_energy_simulation() # simulate REC 
 rec0.save(reference_case) # save results in 'reference_case.pkl'
-
-#%% economic assesment (It has no sense if simulation_years = 1)
+# Actual economic analysis (It has no sense if simulation_years = 1)
 NPV(structure,structure0,study_case,reference_case,economic_data,simulation_years) 
 pp.NPV_plot()
 
 time4 = time.time()  
 
-print('Execution time {:.2f}'.format(time2-time1))
-print('Execution time {:.2f}'.format(time3-time2))
-print('Execution time {:.2f}'.format(time4-time3))
+print('Time to create structure {:.2f} seconds'.format(time2-time1))
+print('Time to run the model {:.2f} seconds'.format(time3-time2))
+print('Time for economic analysis {:.2f} seconds'.format(time4-time3))
 
 ###################################################### work in progress...
 ### version 1.0
