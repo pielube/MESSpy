4e4bb81545b1645a73dfb53793a6860ebd4870d9
Alematiale <alessandro.mati@unifi.it>
2023-06-20 15:01:16 +0200

    minor changes

--- preprocess_test.py
+++ preprocess_test.py
@@ -18,3 +18,11 @@ def change_Elesize(structure,location_name,elesize):
 def change_Htanksize(structure,location_name,tanksize):
     structure[location_name]['H tank']['max capacity'] = tanksize
     return(structure)
+
+def change_electricityprice(energy_market,electricity_price):
+    energy_market['electricity']['purchase'] = electricity_price
+    return(energy_market)
+
+def change_windeleprice(energy_market,windele_price):
+    energy_market['wind electricity']['purchase'] = windele_price
+    return(energy_market)
