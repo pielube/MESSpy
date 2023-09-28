b04268cd78c08875fb40f7036c35fc9bbcc2a20a
Alematiale <alessandro.mati@unifi.it>
2023-09-28 13:57:22 +0200

    upadtes2

--- run_CND_1.py
+++ run_CND_1.py
@@ -9,26 +9,65 @@ import json
 import pickle
 import pandas as pd
 
+# Selecting simulation names
+name_studycase = 'Studicase_GEC_8kW_pv' # str name for energy_balances_results file.pkl
+name_refcase = 'Refcase_GEC_8kW_pv' # str name for energy_balances_results file.pkl
+name_economic = 'From Refcase to Studycase' # str name for economic_assesment_results file.pkl
+
 # Selecting input files:
 path = r'./input_CND' # change the path with r'./input_dev' if you are working on your own run_dev
 file_studycase = 'studycase'
 file_refcase = 'refcase'
 file_general = 'general'
 file_tech_cost = 'tech_cost'
+file_energy_market  = 'energy_market'
+
 
 # Opening input files:
 with open(os.path.join(path,f"{file_studycase}.json"),'r') as f: studycase = json.load(f)
 with open(os.path.join(path,f"{file_refcase}.json"),'r') as f: refcase = json.load(f)
 with open(os.path.join(path,f"{file_general}.json"),'r') as f: general = json.load(f)
 with open(os.path.join(path,f"{file_tech_cost}.json"),'r') as f: tech_cost = json.load(f)
+with open(os.path.join(path,f"{file_energy_market}.json"),'r') as f: energy_market = json.load(f)
 
+#%% ###########################################################################
+"""
+SOLVER - studycase simulation
+======
+"""
+
+rec = REC(studycase,general,file_studycase,file_general,path) # create REC object
+rec.REC_energy_simulation() # simulate REC enegy balances
+rec.tech_cost(tech_cost) # calculate the cost of all technologies 
+rec.save(name_studycase) # save results in 'name_studycase.pkl'
+    
+
+#%% ###########################################################################
+"""
+SOLVER - refcase simulation
+================================
+"""
+  
+# # Reference case simulation (run only if changed)
+# rec0 = REC(refcase,general,file_refcase,file_general,path) # create REC object
+# rec0.REC_energy_simulation() # simulate REC 
+# rec0.tech_cost(tech_cost) # calculate the cost of all technologies 
+# rec0.save(name_refcase) # save results in 'name_refcase.pkl'
+
+    
 # refcase simulation
-name_refcase = 'Without PV' # str name for energy_balances_results file.pkl
 rec0 = REC(refcase,general,file_refcase,file_general,path) # create REC object
 rec0.REC_energy_simulation() # simulate REC 
 rec0.tech_cost(tech_cost) # calculate the cost of all technologies 
 rec0.save(name_refcase) # save results in 'name_refcase.pkl'
 
+#%% ###########################################################################
+"""
+POST PROCESS - Investment assessment
+================================
+"""
+# Net present value calculation to asses the investment comparing refcase and studycase
+# NPV(file_studycase,file_refcase,name_studycase,name_refcase,energy_market,general['simulation years'],path,name_economic)
 # studycase simulation:
 
 scenari = []
